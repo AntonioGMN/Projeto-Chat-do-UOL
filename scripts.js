a95fabac872531ba0095074a9a081f13c3bae5ae
neto <antonio.garciamn@gmail.com>
2021-11-11 15:34:41 -0300

    Requisitos de Entrada na Sala Completos

--- scripts.js
+++ scripts.js
@@ -1,32 +1,71 @@
-const promessa = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages');
-promessa.then(carregarMensagens);
+let promessa = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages');
 
-function carregarMensagens(resposta){
-    console.log(resposta);
-    mensagens = document.querySelector(".mensagens");
-    for(let i=0; i < 100; i++){
-        if(resposta.data[i].type === "status"){
-            mensagens.innerHTML += `<p class="status">
-            ${resposta.data[i].time} ${resposta.data[i].from} ${resposta.data[i].text}
-            </p>`
-        }
-        if(resposta.data[i].type === "message"){
-            mensagens.innerHTML += `<p class="message">
-            ${resposta.data[i].time} ${resposta.data[i].from} para ${resposta.data[i].to}: ${resposta.data[i].text}  
-            </p>`
-        }
-        if(resposta.data[i].type === "private_message"){
-            mensagens.innerHTML += `<p class="private_message">
-            ${resposta.data[i].time} ${resposta.data[i].from} reservadamente para ${resposta.data[i].to}: ${resposta.data[i].text}  
-            </p>`
-        }
+let usuario = prompt("Qual seu nome?");
 
-
+function enviarUsuario(user){
+    const dado = {
+        name: user
     }
-    console.log(resposta.data[0].text)
-    // mensagens.innerHTML += `
-    // <p>${resposta.data[0].time} ${resposta.data[0].from} para ${resposta.data[0].to} ${resposta.data[0].text}</p>`
-    
-
+    const enviar = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', dado);
+    enviar.then(tratarEnvio);
+    enviar.catch(ErroNoEnvio);
 }
 
+function tratarEnvio(resposta){
+    console.log("Envio ok, resposta:");
+    console.log(resposta);
+}
+
+function ErroNoEnvio(resposta){
+    console.log("Erro no Envio:");
+    console.log(resposta);
+    usuario = prompt("Nome em uso. Entre com outro nome");
+    enviarUsuario(usuario);
+}
+
+enviarUsuario(usuario);
+
+function carregarMensagens(){
+    promessa.then(chat);
+    function chat(resposta){
+        //console.log(resposta);
+        mensagens = document.querySelector(".mensagens");
+        mensagens.innerHTML =""
+        for(let i=0; i < resposta.data.length; i++){
+            if(resposta.data[i].type === "status"){
+                mensagens.innerHTML += `<p class="status">
+                ${resposta.data[i].time} ${resposta.data[i].from} ${resposta.data[i].text}
+                </p>`
+            }
+            if(resposta.data[i].type === "message"){
+                mensagens.innerHTML += `<p class="message">
+                ${resposta.data[i].time} ${resposta.data[i].from} para ${resposta.data[i].to}: ${resposta.data[i].text}  
+                </p>`
+            }
+            if(resposta.data[i].type === "private_message"){
+                mensagens.innerHTML += `<p class="private_message">
+                ${resposta.data[i].time} ${resposta.data[i].from} reservadamente para ${resposta.data[i].to}: ${resposta.data[i].text}  
+                </p>`
+            }
+        }
+    }
+}
+
+carregarMensagens();
+
+setInterval(carregarMensagens,3000);
+
+function verificarUsuario(){
+    const dado = {
+        name: usuario
+    }
+    const enviar = axios.post('https://mock-api.driven.com.br/api/v4/uol/status',dado);
+    enviar.then(tratarEnvio);
+    enviar.catch(ErroDeVerificação);
+}
+
+function ErroDeVerificação(resposta){
+    console.log(resposta);
+}
+
+setInterval(verificarUsuario,5000);
